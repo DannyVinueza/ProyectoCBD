14e2b426b7fb69dc53630ec57be4b2883741642a
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-06-11 19:12:47 -0500

    Finalizacion del taller.

---
+++ src/controllers/portafolio.controller.js
@@ -0,0 +1,49 @@
+const Portfolio = require('../models/portafolio')
+
+const renderAllPortafolios = async(req,res)=>{
+    const portfolios = await Portfolio.find({user:req.user._id}).lean()
+    res.render("portafolio/allPortafolios",{portfolios})
+}
+
+const renderPortafolio = (req,res)=>{
+    res.send('Mostrar el detalle de un portafolio')
+}
+const renderPortafolioForm = (req,res)=>{
+    res.render('portafolio/newFormPortafolio')
+}
+
+const createNewPortafolio =async (req,res)=>{
+    const {title, category,description} = req.body
+    const newPortfolio = new Portfolio({title,category,description})
+    newPortfolio.user = req.user._id
+    await newPortfolio.save()
+    res.redirect('/portafolios')
+}
+
+const renderEditPortafolioForm =async(req,res)=>{
+    const portfolio = await Portfolio.findById(req.params.id).lean()
+    res.render('portafolio/editPortfolio',{portfolio})
+}
+
+const updatePortafolio = async(req,res)=>{
+    const portfolio = await Portfolio.findById(req.params.id).lean()
+    if(portfolio.user.toString() !== req.user._id.toString()) return res.redirect('/portafolios')
+    const {title,category,description}= req.body
+    await Portfolio.findByIdAndUpdate(req.params.id,{title,category,description})
+    res.redirect('/portafolios')
+}
+
+const deletePortafolio = async(req,res)=>{
+    await Portfolio.findByIdAndDelete(req.params.id)
+    res.redirect('/portafolios')
+}
+
+module.exports ={
+    renderAllPortafolios,
+    renderPortafolio,
+    renderPortafolioForm,
+    createNewPortafolio,
+    renderEditPortafolioForm,
+    updatePortafolio,
+    deletePortafolio
+}
---
+++ src/models/portafolio.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose')
+
+const portfolioSchema = new Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    description:{
+        type:String,
+        require:true
+    },
+    category :{
+        type:String,
+        require:true
+    },
+    user:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+module.exports = model('portafolio',portfolioSchema)
